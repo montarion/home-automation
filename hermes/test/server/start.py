8b6d9390fdc0f44d03924b12d35156f6b1ed163b
Montarion <jamirograntsaan@gmail.com>
2018-02-22 20:29:58 +0100

    eeey

--- hermes/test/server/start.py
+++ hermes/test/server/start.py
@@ -1,21 +1,31 @@
 from components.server import discover
 from components.tcpserver import tcpcon
 from time import sleep
+from multiprocessing import Process, Queue
+
+
+
 
 clientlist = {}
 
-print(len(clientlist))
+
 def portnumber():
     number = len(clientlist)
     base = 14010
     targetport = base + number
     return targetport
 
+if __name__== '__main__':
+    q1 = Queue()
+    q2 = Queue()
 
-ipaddr = discover().listen()
-clientlist.update(ipaddr=portnumber())
-print(clientlist)
+    
+    ipaddr = discover().listen()
+    clientlist.update(ipaddr=portnumber())
+    print(clientlist)
 
-discover().send(ipaddr, portnumber())
-print('opening tcp')
-tcpcon().accept(portnumber())
+    discover().send(ipaddr, portnumber(), q1)
+    print('opening tcp')
+    p1 = Process(target=tcpcon().accept, args=(q1.get(),))
+
+    p1.start()
